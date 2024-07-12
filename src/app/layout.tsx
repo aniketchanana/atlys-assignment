2727a668090265720ce0df2c0333946ad0fe02bc
aniket <aniket.chanana@gmail.com>
2024-07-13 00:41:52 +0530

    Added animation to dialog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import 'remixicon/fonts/remixicon.css';
+import './animate.css';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Typography from '@/components/ui/Typography';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 interface DialogProps {
@@ -39,34 +39,58 @@ const Dialog: React.FC<DialogProps> = ({
   fullWidth = false,
   maxWidth = 'md',
 }) => {
-  if (!open) return null;
+  const [isVisible, setIsVisible] = useState(open);
+
+  useEffect(() => {
+    if (open) {
+      setIsVisible(true);
+    } else {
+      setTimeout(() => setIsVisible(false), 300);
+    }
+  }, [open]);
+
+  if (!isVisible) return null;
 
   return ReactDOM.createPortal(
-    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50'>
+    <div className='fixed inset-0 z-50 flex items-center justify-center'>
       <div
-        className={`bg-[#1f2937] p-6 rounded-lg shadow-lg w-full ${
-          fullWidth ? 'w-full' : ''
-        } ${getMaxWidthClass(maxWidth)} relative`}
+        className={`h-full w-full relative flex items-center justify-center ${
+          open ? 'overlay-enter' : 'overlay-exit'
+        }`}
       >
         <div
+          className={`absolute top-0 left-0 h-full w-full bg-black bg-opacity-50 ${
+            open ? 'overlay-enter' : 'overlay-exit'
+          }`}
           onClick={onClose}
-          className='absolute top-4 right-4 bg-black p-4 rounded-full w-5 h-5 flex items-center justify-center cursor-pointer'
+        ></div>
+        <div
+          className={`bg-[#1f2937] p-6 rounded-lg shadow-lg w-full ${
+            fullWidth ? 'w-full' : ''
+          } ${getMaxWidthClass(maxWidth)} relative ${
+            open ? 'dialog-enter' : 'dialog-exit'
+          }`}
         >
-          <i className='ri-close-line'></i>
+          <div
+            onClick={onClose}
+            className='absolute top-4 right-4 bg-black p-4 rounded-full w-5 h-5 flex items-center justify-center cursor-pointer'
+          >
+            <i className='ri-close-line'></i>
+          </div>
+          <div className='flex items-center flex-col'>
+            {title && (
+              <Typography variant='caption' className='mb-1 uppercase'>
+                {title}
+              </Typography>
+            )}
+            {subTitle && (
+              <Typography variant='body' className='text-white font-bold mb-6'>
+                {subTitle}
+              </Typography>
+            )}
+          </div>
+          {children}
         </div>
-        <div className='flex items-center flex-col'>
-          {title && (
-            <Typography variant='caption' className='mb-1 uppercase'>
-              {title}
-            </Typography>
-          )}
-          {subTitle && (
-            <Typography variant='body' className='text-white font-bold mb-6'>
-              {subTitle}
-            </Typography>
-          )}
-        </div>
-        {children}
       </div>
     </div>,
     document.body
